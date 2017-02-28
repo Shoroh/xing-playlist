e1118ca6e9e39b0749da8c7cc3f0aa471e79f781
Aleksandr Shelestov <shoroh362@gmail.com>
2017-02-28 18:35:14 +0700

    Playlist model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228110451) do
+ActiveRecord::Schema.define(version: 20170228112300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "playlists", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "playlists", ["user_id"], name: "index_playlists_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 20170228110451) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "playlists", "users"
 end
